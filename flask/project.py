bd1803872e2cbc822a7942de85c7995c90b04f0b
Pavel <grigorevpv@gmail.com>
2018-03-14 23:57:25 +0300

    changed project structure

--- flask/project.py
+++ flask/project.py
@@ -6,4 +6,4 @@ def hello():
     return "<h1 style='color:blue'>Hello from backend one!</h1>"
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port=80)
