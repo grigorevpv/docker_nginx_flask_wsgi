d92a46e4885e977f1b796dca1e11e827f845b098
Pavel Grigoryev <p.grigoryev@corp.mail.ru>
2018-03-15 14:18:30 +0300

    create statsClient

--- flask/project.py
+++ flask/project.py
@@ -1,8 +1,16 @@
+from statsd import StatsClient
+
 from flask import Flask
 application = Flask(__name__)
 
+statsd = StatsClient(host='92.53.91.99',
+                     port=8125,
+                     prefix='web_server_one',
+                     maxudpsize=512)
+
 @application.route("/")
 def hello():
+    statsd.incr('get_request')
     return "<h1 style='color:blue'>Hello from backend one!</h1>"
 
 if __name__ == "__main__":
