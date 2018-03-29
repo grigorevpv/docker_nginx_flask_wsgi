e1bfeb6629ac6fc7103975e88ac07311db7655b8
Pavel Grigoryev <grigorevpv@gmail.com>
2018-03-29 15:24:06 +0300

    change project add cpu loader

--- flask/project.py
+++ flask/project.py
@@ -1,16 +1,21 @@
 from statsd import StatsClient
+from flask.CpuLoader import CpuLoader
 
 from flask import Flask
 application = Flask(__name__)
 
 statsd = StatsClient(host='92.53.91.99',
                      port=8125,
-                     prefix='web_server_one',
+                     prefix='web_server_two',
                      maxudpsize=512)
 
 @application.route("/")
 def hello():
     statsd.incr('get_request')
+
+    loader = CpuLoader(30)
+    loader.load()
+
     return "<h1 style='color:blue'>Hello from backend one!</h1>"
 
 if __name__ == "__main__":
