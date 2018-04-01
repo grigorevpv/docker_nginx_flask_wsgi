8106d149181daaacb5be3545e46e531ea2a743f2
Pavel <grigorevpv@gmail.com>
2018-04-01 19:35:35 +0300

    change git ignore

--- flask_loader/services/CpuLoader.py
+++ flask_loader/services/CpuLoader.py
@@ -1,13 +1,17 @@
+import datetime
+
+from numpy.core import number
+
+
 class CpuLoader(object):
 
-    def __init__(self, load_time: number = None) -> void:
+    def __init__(self, load_time: number = None):
         self._load_time = load_time
 
-    @staticmethod
-    def load(self) -> void:
+    def load(self):
 
         start = datetime.now()
-        delta = timedelta(milliseconds=timeout)
+        delta = datetime.timedelta(milliseconds=self._load_time)
 
         while True:
 
